412712ae14a7b4b4e6dff555426c66d240d2763a
Miguel Rivas Perez <35717991+mrivasperez@users.noreply.github.com>
2020-05-25 21:30:19 -0700

    Added memory and new files

--- cpu.js
+++ cpu.js
@@ -1,5 +1,5 @@
 const createMemory = requires('./create-memory')
-
+const instructions = requires('./instructions')
 
 class CPU {
     constructor(memory) {
@@ -25,4 +25,61 @@ class CPU {
         }
         return this.registers.getUint16(this.registerMap[name]);
     }
+
+    setRegister(name) {
+        if(!(name in this.registerMap)){
+            throw new Error(`setRegister: No such register '${name}'`);
+        }
+        return this.registers.setUint16(this.registerMap[name], value);
+    }
+
+    // Get the instructions that is being pointed to by the ip register
+
+    fetch() {
+        const nextInstructionAddress = this.getRegister('ip');
+        const instruction = this.memory.getUint8(nextInstructionAddress);
+        this.setRegister('ip', nextInstructionAddress + 1);
+        return instruction;
+    }
+
+    fetch16() {
+        const nextInstructionAddress = this.getRegister('ip');
+        const instruction = this.memory.getUint16(nextInstructionAddress);
+        this.setRegister('ip', nextInstructionAddress + 2);
+        return instruction;
+    }
+
+    // Take instruction 
+
+    execute(instruction) {
+        switch (instruction) {
+
+            // Move literal into r1 register
+            case 0x10: {
+                const literal = this.fetch16();
+                this.setRegister('r1', literal);
+            }
+            // Move literal into r2
+            case 0x11: {
+                const literal = this.fetch16();
+                this.setRegister('r2', literal);
+            }
+            //  Add register to register
+            case 0x12: {
+                const r1 = this.fetch();
+                const r2 = this.fetch();
+                const registerValue1 = this.registerMap.getUint16(r1 * 2);
+                const registerValue2 = this.registerMap.getUint16(r2 * 2);
+                this.setRegister('acc', registerValue1 * registerValue2);
+                return;
+            }
+        }
+    }
+
+    step(){
+        const instructions = this.fetch();
+        return this.execute(instruction);
+    }
 }
+
+module.exports = CPU;
